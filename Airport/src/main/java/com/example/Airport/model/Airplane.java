10f4bba5f54b8cbe40f6ce9b45d85786ba0c7648
DavidMijdam <mijdam1996@gmail.com>
2018-10-08 09:59:23 +0200

    [ADD] Airplane model and repository completed. Airplanes contain and id, fuel and a location.

--- Airport/src/main/java/com/example/Airport/model/Airplane.java
+++ Airport/src/main/java/com/example/Airport/model/Airplane.java
@@ -1,4 +1,39 @@
 package com.example.Airport.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Airplane {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private int fuel;
+    private String location; //May become an enum in the future. Keeping it simple for now.
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId (long id) {
+        this.id = id;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel (int fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getLocation () {
+        return location;
+    }
+
+    public void setLocation (String location) {
+        this.location = location;
+    }
 }
--- Airport/src/main/java/com/example/Airport/repository/AirplaneRepository.java
+++ Airport/src/main/java/com/example/Airport/repository/AirplaneRepository.java
@@ -1,4 +1,9 @@
 package com.example.Airport.repository;
 
-public class AirplaneRepository {
+import com.example.Airport.model.Airplane;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirplaneRepository extends CrudRepository<Airplane, Long> {
 }
